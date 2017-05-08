25c9034a253bb668cf1d88b8eb789adc5b23c5b4
Vladimir Markovnin <markovnin@gmail.com>
2017-05-08 17:01:14 +0000

    create validates and references

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,5 @@
 class Question < ApplicationRecord
+  has_and_belongs_to-many :subjects
+  
+  validates :text, presence: true
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,5 @@
 class Subject < ApplicationRecord
+  has_and_belongs_to-many :questions
+  
+  validates :title, presence: true
 end
